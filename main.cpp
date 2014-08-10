9c8bfc18a016b7c04fcbc423e7bcdeccd7377076
nxsupert <ethanmwebb@gmail.com>
2014-08-10 16:13:08 +0100

    Initial Code Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: border2
+ *
+ * Created on 10 August 2014, 16:09
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
